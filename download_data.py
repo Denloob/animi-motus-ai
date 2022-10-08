ecd30e70f11785ed49220cc11d24a523bf5c1225
Denloob <loobdennis@gmail.com>
2022-10-08 20:52:51 +0300

    Add auto dataset downloader

---
+++ download_data.py
@@ -0,0 +1,24 @@
+try:
+    from art import tprint
+
+    tprint("ASL dataset downloader", font="slant")
+except ImportError:
+    print("ASL dataset downloader")
+
+try:
+    import kaggle
+
+    kaggle.api.dataset_download_files(
+        "https://www.kaggle.com/datasets/jonathanoheix/face-expression-recognition-dataset",
+        path="dataset",
+        unzip=True,
+    )
+except IOError as e:
+    if "Could not find kaggle.json." in str(e):
+        print(
+            f'{e}\nread "https://github.com/Kaggle/kaggle-api#api-credentials" to learn how to set up API credentials.'
+        )
+    else:
+        raise e
+else:
+    print("Done!")
